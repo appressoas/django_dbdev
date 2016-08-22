496a6dba6b45c25cf18bcec19e6622e260ac0cc6
Tor Ivar Johansen <tor@torjohansen.no>
2016-08-22 12:59:58 +0200

    dbdev_loaddump: Fixed error in parsing of arguments. Using Options instead of Args

--- django_dbdev/management/commands/dbdev_loaddump.py
+++ django_dbdev/management/commands/dbdev_loaddump.py
@@ -7,9 +7,12 @@ class Command(BaseDbdevCommand):
     help = 'Load a database dump.'
     args = '<dumpfile>'
 
+    def add_extra_arguments(self, parser):
+        parser.add_argument('dumpfile')
+
     def dbdev_handle(self):
-        if len(self.args) != 1:
+        if not self.options.get('dumpfile'):
             self.stderr.write('Dumpfile is required. See --help.')
             raise SystemExit()
-        dumpfile = self.args[0]
+        dumpfile = self.options.get('dumpfile')
         self.dbdev_backend.load_dbdump(dumpfile)
