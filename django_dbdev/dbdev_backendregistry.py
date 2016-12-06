188c6c144a69f4847658a191cd3935b2f5cd633e
Vegard Angell <vegard@appresso.no>
2016-12-06 15:20:44 +0100

    dbdev.backends.postgis: added support for GeoDjango-backend (PostGIS).
    Its basically just postgres, but with some geo-location magics :)

--- django_dbdev/dbdev_backendregistry.py
+++ django_dbdev/dbdev_backendregistry.py
@@ -1,6 +1,7 @@
 from builtins import object
 from django_dbdev.backends.mysql import MySqlBackend
 from django_dbdev.backends.postgres import PostgresBackend
+from django_dbdev.backends.postgis import PostGisBackend
 
 
 class DbdevBackendRegistry(object):
@@ -23,3 +24,4 @@ def register(dbengine, backendclass):
 
 backendregistry.register('django.db.backends.mysql', MySqlBackend)
 backendregistry.register('django.db.backends.postgresql_psycopg2', PostgresBackend)
+backendregistry.register('django.contrib.gis.db.backends.postgis', PostGisBackend)
